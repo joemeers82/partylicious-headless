331146463bcfda25ea6fcdd6c737304015e4908d
Joe Meers Jankowski <joe@Joes-MacBook-Pro-2.local>
2022-12-18 10:33:09 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,22 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Partylicious`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-wordpress',
+    options: {
+      "url": "http://partylicious.wp/graphql"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-postcss", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
